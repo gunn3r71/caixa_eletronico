f608dc352c7d11cd2e074cdd86085a626c629baf
Lucas pereira de oliveira <46692192+gunn3r71@users.noreply.github.com>
2021-02-24 20:49:54 -0300

    Adicionar arquivos de projeto.

---
+++ caixa_eletronico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace caixa_eletronico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
